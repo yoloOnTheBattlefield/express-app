2d871c154f1391c4b0352893684035e4c59565bf
Cristi Florea <cristianflorea1110@yahoo.com>
2017-05-19 14:48:17 +0100

    added time signature

--- models/bear.js
+++ models/bear.js
@@ -1,8 +1,9 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var BearSchema   = new Schema({
-    name: String
+var BearSchema = new Schema({
+    name: String,
+    createdAt: new Date()
 });
 
 module.exports = mongoose.model('Bear', BearSchema);
